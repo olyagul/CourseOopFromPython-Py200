9a69a23c301ab0bbea91bc85e342ccba1a7265b9
olyagul <eternal_olya.gul@mail.ru>
2026-02-03 13:14:52 +0300

    Add files via upload
    Лабораторная работа 2

--- main.py
+++ main.py
@@ -1,30 +1,81 @@
-from classes import Metal, NonMetal, Acid, Salt, Beaker, Flask
+# База данных книг для проверки
+from select import select
 
-def main():
-    print("1. Элементы:")
-    h = Metal("Водород", "H", 1)
-    o = NonMetal("Кислород", "O", 8)
-    print(f"{h} - {h.get_type()}")
-    print(f"{o} - {o.get_type()}")
-    print(f"Водород легкий? {h.is_light()}")
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
 
-    print("\n2. Соединение:")
-    water = Acid("Вода", "H2O", 18.0)
-    salt = Salt("Поваренная соль", "NaCl", 58.5)
-    print(f"{water}")
-    print(f"{salt}")
-    print(f"Масса 2 молей воды: {water.get_mass(2)} г")
 
-    print("\n3. Посуда:")
-    beaker = Beaker("Хим. стакан", 250.0)
-    flask = Flask("Колба", 500.0)
-    beaker.pour("дистиллированная вода", 200.0)
-    flask.pour("раствор кислоты", 300.0)
-    print(f"{beaker}")
-    print(f"{flask}")
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
 
-    flask.empty()
-    print(f"После очистки: {flask}")
 
-if __name__ == "__main__":
-    main()
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+
+
+class Library:
+
+    def __init__(self, books=None):
+        """
+        Не забудьте про 'Конструктор должен принимать необязательный аргумент со значением по умолчанию. Если пользователь
+        его не передал, то библиотека инициализируется с пустым списком книг.'
+        :param books:
+        """
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    def get_next_book_id(self):
+        """
+        Необходимо узнать последнюю книгу в библиотеке, посмотреть атрибут 'id' этой книги и вернуть следующее
+        значение после этого `id`
+        :return:
+        """
+        if not self.books:
+            return 1
+        else:
+            max_id = max(book.id for book in self.books)
+            return max_id + 1
+
+    def get_index_by_book_id(self, id_):
+        """
+        Так как в библиотеке книги хранятся в списке, то данная функция возвращает индекс где книга с определенным
+        `id` хранится в списке книг. Для примера. [Book(id=1, ...), Book(id=2, ...)] книга с id=2 хранится
+        на индексе 1 списка книг
+        :param id_: id книги
+        :return: индекс, где лежит книга в списке книг
+        """
+        for index, book in enumerate(self.books):
+            if book.id == id_:
+                return index
+            raise ValueError
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
